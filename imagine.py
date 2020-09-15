e9fa7c25212a5094cf67cc044e4b6beb665ba9d5
mail@rasmusantons.de <mail@rasmusantons.de>
2020-09-15 04:11:51 +0200

    fix sfw filter

--- imagine.py
+++ imagine.py
@@ -3,7 +3,7 @@ import glob
 
 
 async def rv_image(keyword, adult=False):
-    downloader.download(keyword, limit=1, output_dir='download', adult_filter_off=not adult, force_replace=True, timeout=10)
+    downloader.download(keyword, limit=1, output_dir='download', adult_filter_off=adult, force_replace=True, timeout=10)
     files = glob.glob(f'download/{keyword}/Image_1.*')
     if len(files) > 0:
         return files[0]
