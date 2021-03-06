3250fa4bee994ba4c8d15a2c4253aea60983c589
mail@rasmusantons.de <mail@rasmusantons.de>
2021-03-06 18:56:42 +0100

    update exif command again

--- commands/exif.py
+++ commands/exif.py
@@ -61,12 +61,12 @@ class ExifCommand(Command):
             while '```' in nextline:
                 nextline = nextline.replace('```', '`\u200c`\u200c`')
             text.append(nextline)
-        if len(metadata) == 0:
-            text.append('No selected tags available, use "exiftool all" to see all tags.')
         text.append('```')
+        if not all_tags:
+            text.append('Use "exif all" to see all tags.')
 
         res = '\n'.join(text)
         if len(res) <= 2000:
             await msg.channel.send(res)
         else:
-            await msg.channel.send(text[0], file=discord.File(io.StringIO('\n'.join(text[2:-1])), f'{attachment.filename}.exif.txt'))
+            await msg.channel.send(text[0], file=discord.File(io.StringIO('\n'.join(text[2:-1 if all_tags else -2])), f'{attachment.filename}.exif.txt'))
